ad7feff9cb05c7e8016887dfe4146d28cb3c2e44
Igor Souza <igorsouza.dev@gmail.com>
2019-08-04 07:35:45 -0300

    Adding buttons events

--- src/App.js
+++ src/App.js
@@ -1,38 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Button from './components/Button';
 import Display from './components/Display';
 
-export default function App() {
+export default class App extends Component {
 
   state = {
-    displayValue: '0'
+    displayValue: '0',
+    operation: ''
   };
-
-  return (
-    <View style={styles.container}>
-      <Display value={this.state.displayValue}/>
-      <View style={styles.buttons}>
-        <Button label='AC'></Button>
-        <Button label='/'></Button>
-        <Button label='7'></Button>
-        <Button label='8'></Button>
-        <Button label='9'></Button>
-        <Button label='*'></Button>
-        <Button label='4'></Button>
-        <Button label='5'></Button>
-        <Button label='6'></Button>
-        <Button label='-'></Button>
-        <Button label='1'></Button>
-        <Button label='2'></Button>
-        <Button label='3'></Button>
-        <Button label='+'></Button>
-        <Button label='0'></Button>
-        <Button label='.'></Button>
-        <Button label='='></Button>
+  addDigit = n => {
+    // let newDisplayValue = '';
+    // if(this.state.displayValue === '0') {
+    //   newDisplayValue = n;
+    // } else {
+    //   newDisplayValue += n;
+    // }
+    
+    this.setState({displayValue: n});
+  };
+  clearMemory = () => {
+    this.setState({ displayValue: '0'})
+  };
+  setOperation = operation => {
+    this.setState({operation});
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Display value={this.state.displayValue}/>
+        <View style={styles.buttons}>
+          <Button label='AC' triple onClick={this.clearMemory}></Button>
+          <Button label='/' operation onClick={() => this.setOperation('/')}></Button>
+          <Button label='7' onClick={() => this.addDigit('7')}></Button>
+          <Button label='8' onClick={()=>this.addDigit('8')}></Button>
+          <Button label='9' onClick={()=>this.addDigit('9')}></Button>
+          <Button label='*' operation onClick={() => this.setOperation('*')}></Button>
+          <Button label='4' onClick={() => this.addDigit('4')}></Button>
+          <Button label='5' onClick={() => this.addDigit('5')}></Button>
+          <Button label='6' onClick={() => this.addDigit('6')}></Button>
+          <Button label='-' operation onClick={() => this.setOperation('-')}></Button>
+          <Button label='1' onClick={() => this.addDigit('1')}></Button>
+          <Button label='2' onClick={() => this.addDigit('2')}></Button>
+          <Button label='3' onClick={() => this.addDigit('3')}></Button>
+          <Button label='+' operation onClick={() => this.setOperation('+')}></Button>
+          <Button label='0' double onClick={() => this.addDigit('0')}></Button>
+          <Button label='.' onClick={() => this.addDigit('.')}></Button>
+          <Button label='=' operation onClick={() => this.setOperation('=')}></Button>
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
